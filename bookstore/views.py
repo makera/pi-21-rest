e8395f61062288e7c1c031bd452e42c173ece9c9
User <qwe@mail.ru>
2024-03-07 11:18:47 +0900

    update

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
@@ -13,3 +16,15 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     http_method_names = ('get', 'post', 'put')
+    permission_classes = [DjangoModelPermissions]
+
+    @action(methods=['GET'], detail=False)
+    def bestsellers(self, request):
+        print(request.user)
+        qs = self.get_queryset().filter(year_create__gte=1820)
+        serializer = self.get_serializer_class()(qs, many=True)
+        return Response(data=serializer.data)
+
+    @action(methods=['POST'], detail=True)
+    def take(self, request, pk):
+        return Response(data={'status': 'ok'}, status=200)
